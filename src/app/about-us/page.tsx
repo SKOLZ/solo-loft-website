40d59b6fbb4cbc64299a7a37b6b4b022ff6be18f
SKOLZ <gabriel.zanzotti@gmail.com>
2024-11-18 23:11:37 +0100

    added image support in about us page.

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,6 +1,7 @@
 import { getAboutUs } from "@/services/aboutUs";
 import styles from "./styles.module.scss";
 import { RichText } from "@graphcms/rich-text-react-renderer";
+import Image from "next/image";
 
 interface Props {}
 
@@ -10,7 +11,20 @@ const AboutUsPage: React.FC<Props> = async () => {
     <section className={styles.aboutUsContainer}>
       <h1 className={styles.title}>Sobre nosotros</h1>
       <article className={styles.propertyDescription}>
-        <RichText content={description.raw} />
+        <RichText
+          content={description.raw}
+          renderers={{
+            img: ({ src, altText }) => (
+              <Image
+                src={src!}
+                alt={altText || ""}
+                className={styles.aboutDescriptionImage}
+                width={533}
+                height={300}
+              />
+            ),
+          }}
+        />
       </article>
     </section>
   );
