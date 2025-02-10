d84056087560af4715f0454fd916ae157c54af63
SKOLZ <gabriel.zanzotti@gmail.com>
2025-02-10 19:56:00 +0100

    env cleanup.

--- global.d.ts
+++ global.d.ts
@@ -2,9 +2,6 @@ namespace NodeJS {
   interface ProcessEnv {
     HYGRAPH_BASE_URL: string;
     HYGRAPH_TOKEN: string;
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: string;
-    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: string;
-    RECAPTCHA_SECRET_KEY: string;
     HEROTOFU_API_URL: string;
     NEXT_PUBLIC_TURNSTILE_SITE_KEY: string;
     NEXT_PRIVATE_TURNSTILE_SECRET_KEY: string;
