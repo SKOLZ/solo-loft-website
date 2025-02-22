b32e6f7cc2968005b6eac02c25ca78ed21f2bf66
Gabriel Zanzotti <gabriel.zanzotti@gmail.com>
2025-02-22 23:08:05 +0100

    Responsive (#5)
    * adapted navbar and header to mobile.
    
    * added responsive to all pages.

--- src/app/_components/Header/_components/Navbar/index.tsx
+++ src/app/_components/Header/_components/Navbar/index.tsx
@@ -1,15 +1,24 @@
 "use client";
 
+import React, { useState } from "react";
 import styles from "./styles.module.scss";
-import { usePathname } from "next/navigation";
 import { NavLink } from "./_components/NavLink";
+import { usePathname } from "next/navigation";
 
 interface Props {}
 
 export const Navbar: React.FC<Props> = () => {
   const pathname = usePathname();
+
   return (
     <nav className={styles.navbar}>
+      <div className={styles.hamburger}>
+        <input
+          type="checkbox"
+          className={styles.hamburgerCheckbox}
+          key={pathname}
+        />
+      </div>
       <NavLink
         className={styles.navbarItem}
         activeClassName={styles.active}
