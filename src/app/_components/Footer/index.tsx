b59a43411775e138f387df7449acab1bea082829
SKOLZ <gabriel.zanzotti@gmail.com>
2026-01-28 22:09:51 +0100

    added attribution to SKOLZ.

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import styles from "./styles.module.scss";
-import Image from "next/image";
+import SkolzDesignLogo from "@/assets/skolz_design_logo.svg";
 import ConsultoraRocaLogo from "@/assets/consultora_roca_logo.svg";
 
 interface Props {}
@@ -9,6 +9,13 @@ export const Footer: React.FC<Props> = () => {
   return (
     <footer className={styles.footer}>
       <div className={`container ${styles.footerContainer}`}>
+        <Link
+          href="https://skolz.dev.ar"
+          target="_blank"
+          className={styles.footerAttribution}
+        >
+          <SkolzDesignLogo />
+        </Link>
         <div className={styles.footerGroup}>
           <h3 className={styles.footerGroupTitle}>Empresa y Producto</h3>
           <ul className={styles.footerGroupLinks}>
